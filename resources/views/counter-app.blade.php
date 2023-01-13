9b76a4b4825309c453f0866eb4f7e6c43fdbc30a
linhnv-dev <nvlinh17041992@gmail.com>
2023-01-13 22:53:25 +0700

    01 Giới thiệu và ứng dụng đầu tiên

---
+++ resources/views/counter-app.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+    <div>
+        <h2>Counter App</h2>
+        <livewire:counter /> 
+    </div>
+@endsection
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div class="d-flex">
+    <button class="btn btn-danger" wire:click="decrement">-</button>
+    <h3 class="mx-3">{{ $count }}</h3>
+    <button class="btn btn-info" wire:click="increment">+</button>
+</div>
